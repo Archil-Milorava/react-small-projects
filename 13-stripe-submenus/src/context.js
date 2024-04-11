afacf00e0686551a02e8bbd4ee1e23b3224be24d
Archil-Milorava <achimilorava16@gmail.com>
2024-04-11 22:29:35 +0400

    submenu finnished with context API

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,45 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppConterxt = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenu] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const opneSubmenu = () => {
+    setIsSubmenu(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenu(false);
+  };
+
+  return (
+    <AppConterxt.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        opneSubmenu,
+        openSidebar,
+        closeSidebar,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppConterxt.Provider>
+  );
+};
+
+
+export const useGlobalContext = () => {
+    return useContext(AppConterxt)
+}
